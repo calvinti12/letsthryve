58c9f9359e739d8d90e0cd68bde576b63120e48a
mogh <mogh@example.com>
2016-10-25 19:25:17 -0400

    More detailed error messages

--- app/services/messenger_service.rb
+++ app/services/messenger_service.rb
@@ -4,6 +4,12 @@ class MessengerService
   def initialize(sender_id, input)
     @sender_id = sender_id
     @input = input
+    RestClient.log =
+      Object.new.tap do |proxy|
+        def proxy.<<(message)
+          Rails.logger.info message
+        end
+      end
   end
 
   def route_incoming
@@ -14,14 +20,15 @@ class MessengerService
   def send_generic_template(message, data_hash={})
     data = {
         recipient: {id: @sender_id},
-        message: {attachment: { type: "template"},
-                  payload: {template_type: "generic"},
-                  elements: [{title: "Activities",
-                              item_url: "https://google.com",
-                              image_url: "http://stockfresh.com/files/l/lenm/m/83/1778303_stock-photo-family-exercise.jpg", 
-                              subtitle: "A subtitle thing",
-                              buttons: [{type: "element_share"}]
-                            }]
+        message: {attachment: { type: "template",
+                                payload: {template_type: "generic"},
+                                          elements: [{title: "Activities",
+                                          item_url: "https://google.com",
+                                          image_url: "http://stockfresh.com/files/l/lenm/m/83/1778303_stock-photo-family-exercise.jpg", 
+                                          subtitle: "A subtitle thing",
+                                          buttons: [{type: "element_share"}]
+                                        }]
+                              }
                   }
     }.to_json
     RestClient.post "https://graph.facebook.com/v2.6/me/messages?access_token=#{ENV['PAGE_ACCESS_TOKEN']}",
