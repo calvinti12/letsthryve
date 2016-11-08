c8b1821f3731dc3dd76435adb49fc210b8811df4
mogh <mogh@example.com>
2016-11-08 17:51:23 -0500

    Combine MessageSender and QuickReplySender

--- app/services/route_message_service.rb
+++ app/services/route_message_service.rb
@@ -13,12 +13,16 @@ class RouteMessageService
 
   def catch_all
     random_names = ['Bob', 'Rock', 'Sally', 'Steve']
-    sender = QuickReplySender.new(@sender_id)
+    sender = MessageSender.new(@sender_id)
     sender.set_message('Pick one')
-          .add_reply(title: 'exercise', payload: 'one')
+          .add_reply(title: 'exercise', payload: 'one', image_url: 'fish.png')
           .add_reply(title: 'study', payload: 'two')
           .add_reply(title: 'eat', payload: 'three')
           .deliver!
+    sender = MessageSender.new(@sender_id)
+    sender.set_message('I hope this works')
+          .deliver!
+
   end
 
 end
--- app/services/senders/message_sender.rb
+++ app/services/senders/message_sender.rb
@@ -1,18 +1,52 @@
 class MessageSender < AbstractSender
-
   # How to use:
+  # As a message:
   # sender = MessageSender.new(recipient)
-  # sender.set_message 'hi mom'
-  #     OR
-  # sender.set_message :locale_string_id (and optional data hash)
-  # sender.deliver!
-
-  def set_message(message, data_hash={})
-    @data = {
-      message: {
-        text: message.is_a?(Symbol) ? I18n.t(message, data_hash) : message
-      }
-    }
+  # sender.set_message('Hi mom')
+  #       .deliver!
+  #
+  # As a quick reply:
+  # sender = MessageSender.new(recipient)
+  # sender.set_message('Pick one')
+  #       .add_reply(title: 'exercise', payload: 'one')
+  #       .add_reply(title: 'study', payload: 'two')
+  #       .add_reply(title: 'eat', payload: 'three')
+  #       .deliver!
+  
+  def initialize(recipient)
+    super(recipient)
+    @message = nil
+    @replies = []
   end
 
+  def set_message(message, data_hash={})
+    @message = message.is_a?(Symbol) ? I18n.t(message, data_hash) : message
+    self
+  end
+
+  def add_reply(title: nil, payload: nil, image_url: nil)
+  	raise StandardError('QuickReplySender reply element missing title or payload') unless title && payload
+  	reply = {
+  	  content_type: 'text',
+  	  title: title,
+  	  payload: payload
+  	}
+  	reply[:image_url] = AbstractSender.url_for_asset(image_url) if image_url
+  	@replies.append(reply)
+  	self
+  end
+
+  def deliver
+  	raise StandardError('QuickReplySender message not set') unless @message
+  	raise StandardError('QuickReplySender replies not set properly') unless @replies.size <= 10
+
+  	@data = {
+      recipient: {id: @sender_id},
+      message: {
+      	text: @message
+      }
+    }
+    @data[:message][:quick_replies] = @replies if @replies.size > 0
+    super
+  end
 end
