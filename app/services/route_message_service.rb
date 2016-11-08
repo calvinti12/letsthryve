d37cc707b61c647f4ef14c677d4e2f7447a0a85d
Israel M <purelogiq@gmail.com>
2016-11-07 20:17:40 -0500

    test

--- app/services/route_message_service.rb
+++ app/services/route_message_service.rb
@@ -17,6 +17,26 @@ class RouteMessageService
                        item_url: 'https://google.com', image_url: 'fish.png')
           .add_url_button(title: 'Google.com', url: 'https://google.com', webview_size: 'full')
           .add_share_button
+    sender.add_element(title: 'Test title', subtitle: 'test subtitle',
+                       item_url: 'https://google.com', image_url: 'fish.png')
+          .add_url_button(title: 'Google.com', url: 'https://google.com', webview_size: 'full')
+          .add_share_button
+    sender.add_element(title: 'Test title', subtitle: 'test subtitle',
+                       item_url: 'https://google.com', image_url: 'fish.png')
+          .add_url_button(title: 'Google.com', url: 'https://google.com', webview_size: 'full')
+          .add_share_button
+    sender.add_element(title: 'Test title', subtitle: 'test subtitle',
+                       item_url: 'https://google.com', image_url: 'fish.png')
+          .add_url_button(title: 'Google.com', url: 'https://google.com', webview_size: 'full')
+          .add_share_button
+    sender.add_element(title: 'Test title', subtitle: 'test subtitle',
+                       item_url: 'https://google.com', image_url: 'fish.png')
+          .add_url_button(title: 'Google.com', url: 'https://google.com', webview_size: 'full')
+          .add_share_button
+    sender.add_element(title: 'Test title', subtitle: 'test subtitle',
+                       item_url: 'https://google.com', image_url: 'fish.png')
+          .add_url_button(title: 'Google.com', url: 'https://google.com', webview_size: 'full')
+          .add_share_button
     sender.deliver!
   end
 
