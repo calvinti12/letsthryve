021b559eda2397e2338f75d825fb512479e3a3d5
Israel M <purelogiq@gmail.com>
2016-11-07 20:39:44 -0500

    test

--- app/services/route_message_service.rb
+++ app/services/route_message_service.rb
@@ -18,25 +18,20 @@ class RouteMessageService
           .add_url_button(title: 'Google.com', url: 'https://google.com', webview_size: 'full')
           .add_share_button
     sender.add_element(title: 'Test title', subtitle: 'test subtitle',
-                       item_url: 'https://google.com', image_url: 'fish.png')
-          .add_url_button(title: 'Google.com', url: 'https://google.com', webview_size: 'full')
+                       item_url: 'https://google.com', image_url: 'http://i.annihil.us/u/prod/marvel/i/mg/2/00/53710b14a320b.png')
+          .add_url_button(title: 'Google.com', url: 'https://google.com', webview_size: 'tall')
           .add_share_button
-    sender.add_element(title: 'Test title', subtitle: 'test subtitle',
-                       item_url: 'https://google.com', image_url: 'fish.png')
-          .add_url_button(title: 'Google.com', url: 'https://google.com', webview_size: 'full')
-          .add_share_button
-    sender.add_element(title: 'Test title', subtitle: 'test subtitle',
-                       item_url: 'https://google.com', image_url: 'fish.png')
-          .add_url_button(title: 'Google.com', url: 'https://google.com', webview_size: 'full')
-          .add_share_button
-    sender.add_element(title: 'Test title', subtitle: 'test subtitle',
-                       item_url: 'https://google.com', image_url: 'fish.png')
-          .add_url_button(title: 'Google.com', url: 'https://google.com', webview_size: 'full')
-          .add_share_button
-    sender.add_element(title: 'Test title', subtitle: 'test subtitle',
-                       item_url: 'https://google.com', image_url: 'fish.png')
-          .add_url_button(title: 'Google.com', url: 'https://google.com', webview_size: 'full')
-          .add_share_button
+    sender.deliver!
+
+    sender = MediaSender.new(@sender_id)
+    sender.set_image 'israel.png'
+    sender.deliver!
+
+    sender = PromptSender.new(@sender_id)
+    sender.set_message 'Hi how are you doing today? :)'
+    sender.add_postback_button(title: 'I\'m good :)', payload: 'IM_GOOD_PAYLOAD')
+    sender.add_postback_button(title: 'Umm not good bro...', payload: 'IM_NOT_GOOD_BRO')
+    sender.add_url_button(title: 'Lets Thryve page', url: '/')
     sender.deliver!
   end
 
