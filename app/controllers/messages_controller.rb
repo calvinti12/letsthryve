dd8cd9bfbd6c9283f6aadf145d09f3f54cc1b449
Israel M <purelogiq@gmail.com>
2016-11-13 10:29:22 -0500

    Added structure for receiving messages and postbacks.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,14 +12,44 @@ class MessagesController < ApplicationController
   def incoming
     if params['object'] == 'page'
       params['entry'].each do |entry|
-        if entry['messaging'][0]['message'] # TODO account for multiple messages
-          sender_id = entry['messaging'][0]['sender']['id']
-          text = entry['messaging'][0]['message']['text']
-          messenger_service = RouteMessageService.new(sender_id, text)
-          messenger_service.route_incoming
+        entry['messaging'].each do |item|
+          if item['optin']
+            route_optin(item)
+          elsif item['postback']
+            route_postback(item)
+          elsif item['message'] && item['message']['quick_reply']
+            route_postback(item, item['message']['quick_reply']['payload'])
+          elsif item['message']
+            route_text_message(item)
+          end
         end
       end
     end
     head :ok
+  end
+
+  private
+
+  def route_text_message(item)
+    user = item['sender']['id']
+    text = item['message']['text']
+    receiver = GetStartedReceiver.new(user, {text: text})
+    receiver.get_started
+  end
+
+  def route_postback(item, payload=nil)
+    user = item['sender']['id']
+    payload = item['postback']['payload'] if payload.nil?
+    payload = JSON.parse(payload).deep_symbolize_keys
+    receiver = Object.const_get(payload[:receiver]).new(user, payload[:data])
+    receiver.send(payload[:method].to_sym)
+  end
+
+  def route_optin(item)
+    user = item['sender']['id']
+    payload = item['optin']['ref']
+    sender = MessageSender.new(user)
+    sender.set_message("You did the optin! Wow! #{payload}")
+    sender.deliver!
   end
 end
