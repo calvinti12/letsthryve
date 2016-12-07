c7254890b810a3a2d9214448426d53e899ee31e9
Israel M <purelogiq@gmail.com>
2016-12-07 12:34:09 -0500

    GOO

--- app/services/receivers/get_started_receiver.rb
+++ app/services/receivers/get_started_receiver.rb
@@ -21,23 +21,22 @@ class GetStartedReceiver < AbstractReceiver
 
   def initial_preferences
     multi_message do
-      MessageSender
-        .new(@user)
-        .set_message(:fb_setup_complete, name: @user.first_name, friends_count: @user.friends.count)
-        .deliver!
-
-      PromptSender
-        .new(@user)
-        .set_message(:see_whats_happening)
-        .add_url_button(title: 'What\'s Happening',
-                        url: with_fb_login('/newsfeed', {m_id: @user.fb_messenger_id}),
-                        webview_size: 'full',
-                        use_extensions: true)
-        .add_url_button(title: 'New Invite',
-                        url: with_fb_login('/invites/new', {m_id: @user.fb_messenger_id}),
-                        webview_size: 'full',
-                        use_extensions: true)
-        .deliver!
+      sender = CardSender.new(@user)
+      sender.add_card(title: "What's Happening",
+                      subtitle: "You have #{@user.friends.count} using Let's Thryve! See what they're up to!",
+                      image_url: 'whatshappening.jpg')
+            .add_url_button(url: with_fb_login('/newsfeed', {m_id: @user.fb_messenger_id}),
+                            webview_size: 'full', use_extensions: true, as_default_action: true)
+            .add_url_button(url: with_fb_login('/newsfeed', {m_id: @user.fb_messenger_id}),
+                            webview_size: 'full', use_extensions: true)
+      sender.add_card(title: "Invite a Friend",
+                      subtitle: "Invite a friend to exercise, hangout, go to an event on campus, or anything!",
+                      image_url: 'newinvite.png')
+            .add_url_button(url: with_fb_login('/invites/new', {m_id: @user.fb_messenger_id}),
+                            webview_size: 'full', use_extensions: true, as_default_action: true)
+            .add_url_button(url: with_fb_login('/invites/new', {m_id: @user.fb_messenger_id}),
+                            webview_size: 'full', use_extensions: true)
+      sender.deliver!
     end
   end
 
@@ -50,15 +49,7 @@ class GetStartedReceiver < AbstractReceiver
             .add_url_button(url: with_fb_login('/invites/respond', {invite_id: invite.id}),
                             webview_size: 'tall', as_default_action: true)
             .add_share_button
-      Rails.logger.error("\n\n============================================\n")
-      Rails.logger.error(@user.id)
-      Rails.logger.error("mid: #{@user.fb_messenger_id}")
-      Rails.logger.error("Invitation: #{invite.what}. #{invite.when} - #{invite.where}")
-      Rails.logger.error("#{invite.details}. Click on the image above to respond.")
-      Rails.logger.error(with_fb_login('/invites/respond', {invite_id: invite.id}))
-      Rails.logger.error("\n\nEND========================================\n")
       sender.deliver!
-
     end
   end
 
